8358a1759b5ba327809a79858d6afd6e55f7bbd1
Onur YASAVUL <onuryasavul97@gmail.com>
2022-09-01 10:32:58 +0300

    End of Chapter 3 - A Test Case with an Exception

--- tests/test_math.py
+++ tests/test_math.py
@@ -1,10 +1,23 @@
+import pytest
+
+#A Failing Test Case
 def test_one_plus_one():
     assert 1 + 1 == 2
 
 
+# A Failing Test Case
 def test_one_plus_two():
     a = 1
     b = 2
     c = 0
     # c = 3
     assert a + b == c
+
+
+# A Test Case with an Exception
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError) as e:
+        num = 1 / 0
+
+    assert "division by zero" in str(e.value)
+
