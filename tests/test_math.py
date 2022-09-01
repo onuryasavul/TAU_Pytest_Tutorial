6d51f8f75b62594d7bc6e12dc30989a4cc16c7fc
Onur YASAVUL <onuryasavul97@gmail.com>
2022-09-01 10:57:39 +0300

    End of Chapter 4 - Parameterized Test Cases

--- tests/test_math.py
+++ tests/test_math.py
@@ -21,3 +21,31 @@ def test_divide_by_zero():
 
     assert "division by zero" in str(e.value)
 
+
+# Parameterized Test Cases
+# DRY: Don't Repeat Yourself!
+"""
+def test_multiply_two_positive_ints():
+    assert 2 * 3 == 6
+
+
+def test_multiply_identity():
+    assert 1 * 99 == 99
+
+def test_multiply_zero():
+    assert 0 * 100 == 0
+"""
+
+products = [
+    (2, 3, 6),            # positive integers
+    (1, 99, 99),          # identity
+    (0, 99, 0),           # zero
+    (3, -4, -12),         # positive by negative
+    (-5, -5, 25),         # negative by negative
+    (2.5, 6.7, 16.75),    # floats
+]
+
+
+@pytest.mark.parametrize("a, b, product", products)
+def test_multiplication(a, b, product):
+    assert a * b == product
